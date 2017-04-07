38c71582067aa68df51f93ae08fa11455ba11d1d
detrohutt <detrohutt@gmail.com>
2017-04-07 13:06:26 -0500

    Set webpack client target to 'electron-renderer'

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,5 +5,13 @@ module.exports = {
   electron: {
     width: 1024,
     height: 768
+  },
+  build: {
+    extend (config, { isClient }) {
+      // Extend only webpack config for client-bundle
+      if (isClient) {
+        config.target = 'electron-renderer'
+      }
+    }
   }
 }
