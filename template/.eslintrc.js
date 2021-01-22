a300cc7b565f8894cc1f8df15f938960727ebb02
Gleb Korolkov <gkorolkov@loyalty.com>
2021-01-22 10:53:36 -0500

    Update .eslintrc.js.

--- template/.eslintrc.js
+++ template/.eslintrc.js
@@ -2,23 +2,22 @@ module.exports = {
   root: true,
   env: {
     browser: true,
-    node: true
+    node: true,
   },
   parserOptions: {
-    parser: 'babel-eslint'
+    parser: 'babel-eslint',
   },
   extends: [
-    // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
-    // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
-    'plugin:vue/essential'
+    '@nuxtjs',
+    'prettier',
+    'prettier/vue',
+    'plugin:prettier/recommended',
+    'plugin:nuxt/recommended',
   ],
-  // required to lint *.vue files
-  plugins: [
-    'vue'
-  ],
+  plugins: ['prettier'],
   // add your custom rules here
   rules: {
-	"indent": ["error", "tab"],
-  	"no-tabs": 0
-  }
+    'no-console': 'off',
+    'nuxt/no-cjs-in-config': 'off',
+  },
 }
