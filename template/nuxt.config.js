6141cab6e901a8848e5ff33b18a6b8a8aae771fc
Gleb Korolkov <gkorolkov@loyalty.com>
2021-01-22 10:48:41 -0500

    Update nuxt.config.js.

--- template/nuxt.config.js
+++ template/nuxt.config.js
@@ -1,24 +1,50 @@
 module.exports = {
-	mode: 'spa',
-	head: {title: '{{name}}'}, // Headers of the page
-	loading: false, // Disable default loading bar
-	build: {
-		extend (config, { isDev, isClient }) {
-			if (isDev && isClient) {
-				// Run ESLint on save
-				config.module.rules.push({
-					enforce: 'pre',
-					test: /\.(js|vue)$/,
-					loader: 'eslint-loader',
-					exclude: /(node_modules)/
-				})
-			}
-			// Extend only webpack config for client-bundle
-			if (isClient) { config.target = 'electron-renderer' }
-		}
-	},
-	dev: process.env.NODE_ENV === 'DEV',
-	css: [
-		'@/assets/css/global.css'
-	]
+  // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
+  ssr: false,
+
+  // Global page headers: https://go.nuxtjs.dev/config-head
+  head: {
+    title: '{{ name }}',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+    ],
+  },
+
+  // Development mode flag: https://nuxtjs.org/docs/2.x/directory-structure/nuxt-config
+  dev: process.env.NODE_ENV === 'DEV',
+
+  // Global CSS: https://go.nuxtjs.dev/config-css
+  css: ['@/assets/css/global.css'],
+
+  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+  plugins: [],
+
+  // Auto import components: https://go.nuxtjs.dev/config-components
+  components: true,
+
+  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+  buildModules: [
+    // https://go.nuxtjs.dev/eslint
+    '@nuxtjs/eslint-module',
+  ],
+
+  // Modules: https://go.nuxtjs.dev/config-modules
+  modules: [],
+
+  // Build Configuration: https://go.nuxtjs.dev/config-build
+  build: {
+    extend(config, { isDev, isClient }) {
+      // Extend only webpack config for client-bundle
+      if (isClient) {
+        config.target = 'electron-renderer'
+      }
+    },
+  },
+
+  // Loading component options: https://nuxtjs.org/docs/2.x/directory-structure/nuxt-config#loading
+  loading: false,
+
+  // Telemetry collection feature toggle
+  telemetry: false,
 }
