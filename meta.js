407ad16f39d20b29e75e63d05f3734baa89e1582
Gleb Korolkov <gkorolkov@loyalty.com>
2021-01-22 11:02:03 -0500

    Whitespace changes in meta.js.

--- meta.js
+++ meta.js
@@ -1,30 +1,30 @@
 module.exports = {
-	helpers: {
-		escape: function(value) {
-			return value.replace(/'/g, '&apos;');
-		}
-	},
-	prompts: {
-		name: {
-			'type': 'string',
-			'required': true,
-			'message': 'Project name'
-		},
-		description: {
-			'type': 'string',
-			'required': false,
-			'message': 'Project description',
-			'default': 'Nuxt + Electron'
-		},
-		author: {
-			'type': 'string',
-			'message': 'Author'
-		},
-		appId: {
-			'type': 'string',
-			'required': true,
-			'message': 'App Id (e.g. com.example.app)'
-		}
-	},
-	completeMessage: '{{#inPlace}}To get started:\n\n  npm install # Or yarn\n  npm run dev{{else}}To get started:\n\n  cd {{destDirName}}\n  npm install # Or yarn\n  npm run dev{{/inPlace}}'
+  helpers: {
+    escape: function(value) {
+      return value.replace(/'/g, '&apos;');
+    }
+  },
+  prompts: {
+    name: {
+      'type': 'string',
+      'required': true,
+      'message': 'Project name'
+    },
+    description: {
+      'type': 'string',
+      'required': false,
+      'message': 'Project description',
+      'default': 'Nuxt + Electron'
+    },
+    author: {
+      'type': 'string',
+      'message': 'Author'
+    },
+    appId: {
+      'type': 'string',
+      'required': true,
+      'message': 'App Id (e.g. com.example.app)'
+    }
+  },
+  completeMessage: '{{#inPlace}}To get started:\n\n  npm install # Or yarn\n  npm run dev{{else}}To get started:\n\n  cd {{destDirName}}\n  npm install # Or yarn\n  npm run dev{{/inPlace}}'
 };
